c43f5f9774ce1ee41ca5771c437fb8b13093161b
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-13 19:52:47 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bdlz/dssq/StackQueueMain.java
@@ -0,0 +1,8 @@
+package com.bdlz.dssq;
+
+
+public class StackQueueMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the data Structure in Stack and Queue Program");
+    }
+}
